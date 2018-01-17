2977769f71fa567c26531aff094700ac978a339f
zurfyx <zurfyx@gmail.com>
2018-01-17 19:48:47 +0100

    scrolling view demo

---
+++ example/app/default.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: 'default.component.html',
+  styleUrls: ['default.component.scss'],
+})
+export class DefaultComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
